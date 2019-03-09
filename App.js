7a262d6602bd3996348b448105c6404a50f56b10
ray <364664251@qq.com>
2019-03-09 21:26:40 +0800

    隐藏状态栏

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {createStackNavigator, createAppContainer} from 'react-navigation';
 import HomeScreen from './js/screen/HomeScreen'
 import TestScreen from './js/screen/TestScreen'
 import LoginScreen from './js/screen/LoginScreen'
+import SplashScreen from "./js/screen/SplashScreen";
 
 
 //全局存储
@@ -27,33 +28,36 @@ import './js/utils/Global'
 // });
 
 const MainNavigator = createStackNavigator({
+    Splash: {screen: SplashScreen},
     Home: {screen: HomeScreen},
     Test: {screen: TestScreen},
-    Login: {screen: LoginScreen}
+    Login: {screen: LoginScreen},
+
 }, {
-    initialRouteName: 'Home', // 默认显示界面
-    navigationOptions: {  // 屏幕导航的默认选项, 也可以在组件内用 static navigationOptions 设置(会覆盖此处的设置)
-        headerStyle: {elevation: 0, shadowOpacity: 0, height: 48, backgroundColor: "#2196f3"},
-        headerTitleStyle: {color: '#fff', fontSize: 16}, //alignSelf:'center'  文字居中
-        headerBackTitleStyle: {color: '#fff', fontSize: 12},
-        // headerTintColor:{},
-        gesturesEnabled: true,//是否支持滑动返回收拾，iOS默认支持，安卓默认关闭
-    },
-    mode: 'card',  // 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
-    headerMode: 'screen', // 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
-    onTransitionStart: (Start) => {
-        console.log('导航栏切换开始');
-    },  // 回调
-    onTransitionEnd: () => {
-        console.log('导航栏切换结束');
-    }  // 回调
+    // initialRouteName: 'Splash', // 默认显示界面
+    // navigationOptions: {  // 屏幕导航的默认选项, 也可以在组件内用 static navigationOptions 设置(会覆盖此处的设置)
+    //     headerStyle: {elevation: 0, shadowOpacity: 0, height: 48, backgroundColor: "#2196f3"},
+    //     headerTitleStyle: {color: '#fff', fontSize: 16}, //alignSelf:'center'  文字居中
+    //     headerBackTitleStyle: {color: '#fff', fontSize: 12},
+    //     // headerTintColor:{},
+    //     gesturesEnabled: true,//是否支持滑动返回收拾，iOS默认支持，安卓默认关闭
+    //
+    // },
+    // mode: 'card',  // 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
+    // headerMode: 'screen', // 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
+    // onTransitionStart: (Start) => {
+    //     console.log('导航栏切换开始');
+    // },  // 回调
+    // onTransitionEnd: () => {
+    //     console.log('导航栏切换结束');
+    // }  // 回调
 });
 
 const defaultGetStateForAction = MainNavigator.router.getStateForAction;
 
 MainNavigator.router.getStateForAction = (action, state) => {
     //页面是MeScreen并且 global.user.loginState = false || ''（未登录）
-    if (action.routeName === 'Test' && (!global.user || !global.user.loginState)) {
+    if (action.routeName === 'Home' && (!global.user || !global.user.loginState)) {
         this.routes = [
             ...state.routes,
             {key: 'id-' + Date.now(), routeName: 'Login', params: {name: 'name1'}},
---
+++ js/screen/SplashScreen.js
@@ -0,0 +1,53 @@
+import React, {Component, PropTypes} from 'react';
+import {
+    View,
+    Animated,
+    StatusBar,
+    Dimensions,
+} from 'react-native';
+import BaseScreen from './BaseScreen'
+
+let WINDOW_WIDTH = Dimensions.get('window').width;
+
+
+export default class SplashScreen extends BaseScreen {
+
+    ae() {
+        this.props.navigation.navigate('Home')
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fadeAnim: new Animated.Value(0.4),
+        };
+    }
+
+    componentDidMount() {
+        Animated.timing(
+            this.state.fadeAnim,
+            {
+                toValue: 1,
+                duration: 1500,
+            }
+        ).start(() => {
+            //动画执行完毕
+            this.ae()
+        });
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <StatusBar backgroundColor="blue" barStyle="light-content"/>
+                <View>
+                    <StatusBar hidden={true}/>
+                </View>
+                <Animated.Image style={{flex: 1, width: WINDOW_WIDTH, height: null, opacity: this.state.fadeAnim}}
+                                source={require('../../img/sp.jpg')}/>
+            </View>
+
+        );
+    }
+
+}
